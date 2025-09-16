2351c668f8aacc1b66a208d0fca6a545845f1e64
aishaadam <aishaadam3687@example.com>
2025-09-16 13:20:39 +0100

    second commit

--- src/app/component/ServiceCard.jsx
+++ src/app/component/ServiceCard.jsx
@@ -1,20 +1,23 @@
-export default-funtion ServiceCard() {
-
-    return(
-        <div className="container">
-        <div className="row">
-            <div className="col-lg-4">
-                <div class="card">
-  <img src="/img.jpg" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+export default function ServiceCard() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div class="card">
+            <img src="/img.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
             </div>
-
+          </div>
         </div>
-        </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
